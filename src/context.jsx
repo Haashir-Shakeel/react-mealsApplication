6b8c819f70b4d1d443f602bbb30457b4f053c69e
Haashir-Shakeel <david10james99@gmail.com>
2022-10-03 14:14:36 +0500

    installed axios, fetching in context

--- src/context.jsx
+++ src/context.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import axios from 'axios'
 
 export const AppContext = React.createContext()
 
 
 export const AppProvider = ({children}) => {
+
+    const randomMealsUrl = 'https://www.themealdb.com/api/json/v1/1/random.php#'
+    const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a#'
+
+    const fetchMeal =async (url) => {
+        try{
+            const {data} = await axios(url) //destructuring data from respons
+    console.log(data)
+        }catch(error){
+            console.log(error.response)
+        }
+    
+    }
+
+    useEffect(()=>{
+        fetchMeal(allMealsUrl)
+    },[])
     return (
         <AppContext.Provider value="hello">
             {children}
