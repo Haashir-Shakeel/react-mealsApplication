b3adca90328b5b5af5692067fc4f51736bf67b5b
Haashir-Shakeel <david10james99@gmail.com>
2022-10-12 18:33:13 +0500

    Meals component styling

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useGlobalContext } from '../context'
+import {BsHandThumbsUp} from 'react-icons/bs'
+
 const Meals = () => {
   const {meals} = useGlobalContext()
   
@@ -10,10 +12,10 @@ const Meals = () => {
         const {idMeal, strMeal: title, strMealThumb: image} = singleMeal
 
         return <article className='single-meal' key={idMeal}>
-          <img src={image} className="img" style={{width:'200px'}}/>
+          <img src={image} className="img" alt={title}/>
           <footer>
             <h5>{title}</h5>
-            <button className='like-btn'>click me</button>
+            <button className='like-btn'><BsHandThumbsUp/></button>
           </footer>
           </article>
       })}
